26707ea4ef2d717157f5e8b316d4277c8067dc0f
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-08-05 03:16:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,9 @@ second_number = 1
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
+<<<<<<< HEAD
 quotient = first_number/second_number
+=======
+quotient = first_number/second_number
+
+>>>>>>> 1a3e8866cc874ef313b23deacb9251199bcc9d7e
